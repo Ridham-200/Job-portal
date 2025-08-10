040dce210134ceb6dcdbf52bfc521cf1fa20d478
Ridham-200 <158192133+Ridham-200@users.noreply.github.com>
2025-08-11 01:23:56 +0530

    dep_org

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,11 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 dotenv.config({});
 
 const app = express();
-
+const _dirname=path.resolve();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -26,8 +27,14 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
-app.get('/', (req, res) => {
-    res.send('Job Portal API is running...');
+// app.get('/', (req, res) => {
+//     res.send('Job Portal API is running...');
+// });
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get('*',(_,res)=>
+{
+res.sendFile(path.resolve(_dirname,"frontend","dist","index.html"))
 });
 
 app.listen(PORT,()=>{
